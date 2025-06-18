ed7a863e327cb7eb0f52aa61cf4bda09aeb25a93
chuck <chuck.a.black@gmail.com>
2025-06-18 18:02:59 -0400

    bedrock no destroy and trees

--- minecraft-like.py
+++ minecraft-like.py
@@ -14,6 +14,8 @@ textures = {
 sky_bg = load_texture("assets/textures/Sky.png")
 build_sound = Audio("assets/sounds/Build_Sound.wav", loop=False, autoplay=False)
 
+block_pick = 1
+
 class Block(Button):
     def __init__(self, position=(0,0,0), texture=textures[1], breakable=True):
         super().__init__(
@@ -28,6 +30,15 @@ class Block(Button):
         )
         self.breakable = breakable
 
+    def input(self, key):
+        if self.hovered:
+            if key == "left mouse down":
+                build_sound.play()
+                new_block = Block(position=self.position + mouse.normal, texture=textures[block_pick])
+            elif key == "right mouse down" and self.breakable:
+                build_sound.play()
+                destroy(self)
+
 class Sky(Entity):
     def __init__(self):
         super().__init__(
@@ -38,6 +49,25 @@ class Sky(Entity):
             double_sided=True
         )
 
+class Tree(Entity):
+    def __init__(self, position=(0,0,0)):
+        super().__init__(
+            parent=scene,
+            position=position,
+            model="assets/models/Lowpoly_tree_sample.obj",
+            scale=(0.5,0.5,0.5),
+            collider="mesh",
+        )
+
+def generate_trees(num_trees=3, terrain_size=20):
+    for _ in range(num_trees):
+        x = random.randint(0, terrain_size-1)
+        y = 0
+        z = random.randint(0, terrain_size-1)
+        Tree(position=(x,y,z))
+
+generate_trees()
+
 for z in range(20):
     for x in range(20):
         block = Block(position=(x,0,z))
@@ -46,8 +76,18 @@ for z in range(20):
 
 
 def update():
+    global block_pick
+
+    for i in range(1,6):
+        if held_keys[str(i)]:
+            block_pick = i
+            break
+
     if held_keys["escape"]:
         application.quit()
+
+    if player.y <= -5:
+        player.position = (10,10,10)
 
 
 
